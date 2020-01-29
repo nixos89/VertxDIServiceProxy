cb2bfb1c197864254a3bb2dae3e93371840f1cbf
nixos89 <nixos89@gmail.com>
2020-01-29 23:51:21 +0100

    minor fix

--- vertx-di-service-proxy/src/main/java/com/medium/frederik/di/Main.java
+++ vertx-di-service-proxy/src/main/java/com/medium/frederik/di/Main.java
@@ -2,10 +2,11 @@ package com.medium.frederik.di;
 
 import io.vertx.core.Launcher;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		Launcher.executeCommand("run", LauncherVerticle.class.getName());
 	}
-		
+
 }
