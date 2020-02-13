f3727f61761ded9b4ff8a3408b306e1723e4b489
nixos89 <nixos89@gmail.com>
2020-02-13 14:08:34 +0100

    minor fix

---
+++ vertx-pg-reactive-plain/src/main/java/com/ns/vertx/pgplain/DBQueries.java
@@ -0,0 +1,10 @@
+package com.ns.vertx.pgplain;
+
+public class DBQueries {
+	public static String CREATE_CATEGORY_TABLE_SQL = "CREATE TABLE IF NOT EXISTS Category (category_id BIGSERIAL PRIMARY KEY, name VARCHAR(30), is_deleted boolean);";	
+	public static String GET_ALL_CATEGORIES_SQL = "SELECT category_id, name, is_deleted FROM Category;";
+	public static String GET_CATEGORY_BY_ID_SQL = "SELECT category_id, name, is_deleted FROM Category WHERE category_id = $1;";
+	public static String CREATE_CATEGORY_SQL = "INSERT INTO Category (name, is_deleted) VALUES($1, $2) RETURNING category_id;";
+	public static String UPDATE_CATEGORY_SQL = "UPDATE Category SET name = $1, is_deleted = $2 WHERE category_id = $3 RETURNING category_id;"; 
+	public static String DELETE_CATEGORY_BY_ID_SQL = "DELETE Category WHERE category_id = $1;";
+}
---
+++ vertx-pg-reactive-plain/src/main/java/com/ns/vertx/pgplain/MainVerticle.java
@@ -0,0 +1,247 @@
+package com.ns.vertx.pgplain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowIterator;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.Tuple;
+
+import static com.ns.vertx.pgplain.ActionHelper.*;
+import static com.ns.vertx.pgplain.DBQueries.*;
+
+public class MainVerticle extends AbstractVerticle {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+	
+	private PgPool pgClient; 
+	
+	@Override
+	public void start(Promise<Void> startPromise) throws Exception {				
+		Router routerREST = Router.router(vertx);
+		routerREST.post("/*").handler(BodyHandler.create());
+		routerREST.get("/categories").handler(this::getAllCategoriesHandler);
+		routerREST.get("/categories/:id").handler(this::getCategoryByIdHandler);
+		routerREST.delete("/categories/:id").handler(this::deleteCategoryHandler);
+		routerREST.post().handler(BodyHandler.create());
+		routerREST.post("/categories").handler(this::createCategoryHandler);
+		routerREST.put("/categories/:id").handler(this::updateCategoryHandler);
+		Router routerAPI = Router.router(vertx);
+		routerAPI.mountSubRouter("/api", routerREST);
+		
+		ConfigRetriever retriever = ConfigRetriever.create(vertx);		
+		retriever.getConfig(h -> {
+			if (h.succeeded()) {
+				JsonObject hResult = h.result();
+//				LOGGER.info(" ======== hResult ========= " + hResult.encodePrettily() + " =================");
+			} else {
+				LOGGER.error("Somethin' 's fucked up!");
+			}
+		});
+		
+		PgConnectOptions connectOptions = new PgConnectOptions()
+				.setPort(5432).setHost("localhost")
+				.setDatabase("vertx-pg-plain-db")
+				.setUser("postgres").setPassword("postgres");
+		
+		PoolOptions poolOptions = new PoolOptions().setMaxSize(30);
+		pgClient = PgPool.pool(vertx, connectOptions, poolOptions);
+		
+		Future<Void> futureConnection = connect().compose(connection -> {
+			Future<Void> retFuture = Future.future(); // TODO: change it later to Promise when everything works!
+			createTableIfNeeded(connection).future()
+				.setHandler(x -> {
+					connection.close();
+					retFuture.handle(x.mapEmpty());
+				});
+			return retFuture;
+		});
+		
+		futureConnection.compose(v -> createHttpServer(pgClient, routerAPI))
+						.setHandler(startPromise);
+	
+	}// start::END
+	
+	public Future<SqlConnection> connect() {
+		Promise<SqlConnection> promise = Promise.promise();
+		pgClient.getConnection(ar -> {
+			if (ar.succeeded()) {
+				promise.handle(ar.map(connection -> connection));
+			}
+		});
+		return promise.future();
+	}
+	
+	
+	public Future<Void> createHttpServer(PgPool pgClient, Router router) {
+		Promise<Void> promise = Promise.promise();
+		vertx.createHttpServer()
+			 .requestHandler(router)
+			 .listen(8080, res -> promise.handle(res.mapEmpty()));
+		return promise.future();
+	}
+
+	
+	private Promise<SqlConnection> createTableIfNeeded(SqlConnection connection) {
+		Promise<SqlConnection> promise = Promise.promise();
+		pgClient.getConnection(ar1 -> {
+			if (ar1.succeeded()) {
+				LOGGER.info("Connected!");
+				SqlConnection conn = ar1.result();		
+				conn.query(CREATE_CATEGORY_TABLE_SQL, ar2 -> {
+					if (ar2.succeeded()) {
+						promise.handle(ar2.map(conn));
+					} else {
+						LOGGER.error("Error, executing 'create_category_table_sql' query failed!", ar2.cause());
+						conn.close();
+					}
+				});
+			} else {
+				LOGGER.error("Error, acquiring DB connection! Cause: ", ar1.cause());
+			}
+		});	
+		return promise;
+	}
+	
+	private JsonObject convertRowSetToJsonObject(RowSet<Row> rs) {
+		JsonObject categories = new JsonObject();
+		JsonObject category = new JsonObject();
+		RowIterator<Row> ri = rs.iterator();
+		JsonArray ja = new JsonArray();
+		while(ri.hasNext()) {
+			Row row = ri.next();
+			category.put("category_id", row.getLong(0));
+			category.put("name", row.getString(1));
+			category.put("is_deleted", row.getBoolean(2));
+			ja.add(category);							
+			LOGGER.info("category = " + category.encodePrettily());
+			category = new JsonObject();
+		}
+		categories.put("categories", ja);
+		return categories;
+	}
+	
+	private void getAllCategoriesHandler(RoutingContext rc) {
+		pgClient.getConnection(ar -> {
+			if (ar.succeeded()) {
+				SqlConnection sqlConnection = ar.result();
+				sqlConnection.query(GET_ALL_CATEGORIES_SQL, fetch -> {
+					if (fetch.succeeded()) {
+						RowSet<Row> rs = fetch.result();
+						JsonObject categories = convertRowSetToJsonObject(rs);
+						rc.response().setStatusCode(303);
+						rc.response().putHeader("Content-Type", "application/json; UTF-8");
+						rc.response().end(categories.encodePrettily());
+						
+						sqlConnection.close();
+					} else {
+						LOGGER.error("Error, connection not established! Cause: ", fetch.cause());
+						sqlConnection.close();
+					}
+				});
+			}
+		});
+	}
+	
+	private void getCategoryByIdHandler(RoutingContext rc) {
+		pgClient.getConnection(ar -> {
+			if (ar.succeeded()) {
+				SqlConnection conn = ar.result();
+				conn.preparedQuery(GET_CATEGORY_BY_ID_SQL, Tuple.of(Integer.valueOf(rc.request().getParam("id"))), 
+						arCID -> {
+					if (arCID.succeeded()) {						
+						Row row = arCID.result().iterator().next();
+						JsonObject category = new JsonObject()
+								.put("category_id", row.getLong(0))
+								.put("name", row.getString(1))
+								.put("is_deleted", row.getBoolean(2));
+						
+						LOGGER.info("Succeeded in quering category by id! Result:");
+						rc.response().setStatusCode(200);
+						rc.response().putHeader("Content-Type", "application/json; UTF-8");
+						rc.response().end(category.encodePrettily());
+					} else {
+						LOGGER.error("Error, failed! Cause: ", arCID.cause());
+					}
+					conn.close();
+				});
+			} else {
+				LOGGER.error("DB connection NOT obtained!", ar.cause());
+			}
+		});
+	}
+	
+	private void updateCategoryHandler(RoutingContext rc) {
+		int id = Integer.valueOf(rc.request().getParam("id"));
+		Category category = rc.getBodyAsJson().mapTo(Category.class);
+		connect().compose(connection -> updateCategory(connection, category, id))
+				 .setHandler(noContent(rc));
+	}
+	
+	private Future<Void> updateCategory(SqlConnection connection, Category category, int id) {
+		Promise<Void> promise = Promise.promise();
+		connection.preparedQuery(UPDATE_CATEGORY_SQL, Tuple.of(category.getName()), update -> {
+			
+		});
+		
+		return promise.future();
+	}
+	
+	private void createCategoryHandler(RoutingContext rc) {
+		Category category = rc.getBodyAsJson().mapTo(Category.class);
+		connect().compose(conn -> createCategory(conn, category, true))
+				 .setHandler(created(rc));			
+	}
+	
+	private Future<Category> createCategory(SqlConnection conn, Category category, boolean closeConnection) {
+		Promise<Category> promise = Promise.promise();
+		conn.preparedQuery(CREATE_CATEGORY_SQL, Tuple.of(category.getName(), category.getIsDeleted()), save -> {
+			if (closeConnection) {
+				conn.close();
+			}			
+			int id = save.result().iterator().next().getInteger(0);						
+			Category cat = new Category(id, category.getName(), category.getIsDeleted());
+			
+			promise.handle(Future.succeededFuture(cat));
+		});
+		return promise.future();
+	}
+	
+	
+	private void deleteCategoryHandler(RoutingContext rc) {
+		int id = Integer.valueOf(rc.request().getParam("id"));
+		pgClient.getConnection(ar -> {
+			if (ar.succeeded()) {
+				SqlConnection sqlConnection = ar.result();
+				sqlConnection.preparedQuery(DBQueries.DELETE_CATEGORY_BY_ID_SQL, Tuple.of(id), fetch -> {
+					if (fetch.succeeded()) {
+						LOGGER.info("Category with id = " + id + " has been successfully deleted!");
+						rc.response().setStatusCode(204);
+						rc.response().putHeader("Content-type", "application/json: UTF-8");
+						rc.response().end();						
+					} else {
+						LOGGER.error("Error, connection not established! Cause: ", fetch.cause());
+					}					
+				});
+				sqlConnection.close();
+			} else {
+				LOGGER.error("DB connection NOT obtained!", ar.cause());
+			}
+		});
+	}
+	
+}
